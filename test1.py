08bc0d6097b639d40b0d64a932673d559cce11c3
Nova <hafizh.naufal2001@gmail.com>
2025-07-31 12:23:05 +0200

    Add files via upload
    test for Project

---
+++ test1.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Trossen Robotics
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+
+from interbotix_common_modules.common_robot.robot import robot_shutdown, robot_startup
+from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
+import numpy as np
+
+
+"""
+This script makes the end-effector perform pick, pour, and place tasks.
+Note that this script may not work for every arm as it was designed for the wx250.
+Make sure to adjust commanded joint positions and poses as necessary.
+
+To get started, open a terminal and type:
+
+    ros2 launch interbotix_xsarm_control xsarm_control.launch.py robot_model:=wx250s
+
+Then change to this directory and type:
+
+    python3 bartender.py
+"""
+
+
+def main():
+    bot = InterbotixManipulatorXS(
+        robot_model='wx250s',
+        group_name='arm',
+        gripper_name='gripper',
+    )
+
+    robot_startup()
+
+    if (bot.arm.group_info.num_joints < 5):
+        bot.get_node().logfatal(
+            'This demo requires the robot to have at least 5 joints!'
+        )
+        robot_shutdown()
+        sys.exit()
+        '''
+
+    bot.arm.set_single_joint_position(joint_name='waist', position=np.pi/2.0)
+    # Raise the shoulder
+    bot.arm.set_single_joint_position(joint_name='shoulder', position=0.5)
+    
+    # Bend the elbow
+    bot.arm.set_single_joint_position(joint_name='elbow', position=1.2)
+    
+    # Move wrist pitch
+    bot.arm.set_single_joint_position(joint_name='wrist_angle', position=0.8)
+    
+    # Rotate wrist
+    bot.arm.set_single_joint_position(joint_name='wrist_rotate', position=-1.0)
+    
+    # Open or close gripper (usually 0.0 closed, positive to open)
+    bot.gripper.get_gripper_position(position=0.02)	
+    '''
+    
+    #bot.arm.set_ee_cartesian_trajectory(x=0.2)
+    
+    for i in range(3):
+	    
+	    bot.arm.set_joint_positions([0.0, -0.4, 1.0, 0.0, 0.5, 0.0])
+	    
+	    bot.arm.set_joint_positions([0.0, 0.0, 1.0, 0.0, -0.5, 0.0])
+	    bot.arm.set_joint_positions([0.0, 0.0, 1.0, 0.0, -1.0, 0.0])
+	    
+	    
+	    bot.arm.set_joint_positions([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+	   
+	    #bot.arm.set_joint_positions([0.0, 0.1, 0.8, 0.0, -0.9, 0.0])
+	    
+    #bot.arm.set_single_joint_position(joint_name='shoulder', position=0.5)
+    
+    
+    # Done
+    #bot.arm.go_to_home_pose()
+    #bot.arm.go_to_sleep_pose()	
+    
+    
+    
+    
+    
+    robot_shutdown()
+
+
+if __name__ == '__main__':
+    main()
